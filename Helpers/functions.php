f276c485ff32a8aedecc77c7e129e07d83e6f8ae
Raphitpt <tiphonetraphi@gmail.com>
2023-06-15 20:35:04 +0200

    fix bug + add manifest for pwa

--- Helpers/functions.php
+++ Helpers/functions.php
@@ -23,8 +23,7 @@ function head(string $title = ''): string
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
- 
-  <link rel="stylesheet" href="./assets/css/style.css">
+  <link rel="manifest" href="../manifest.webmanifest" />
   <title>$title</title>
   <link rel="stylesheet" href="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.css" />
     <script src="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.js"></script>
@@ -55,7 +54,7 @@ function isPostMethod(): bool
 function calendar($group = ''){
     $calendar_link = '';
     if($group == "BUT1-TP1"){
-        $calendar_link ='http://upplanning.appli.univ-poitiers.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=21314&projectId=13&calType=ical&nbWeeks=15';
+        $calendar_link ='https://upplanning.appli.univ-poitiers.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=24838&projectId=13&calType=ical&nbWeeks=15';
     }
     if($group == "BUT1-TP2"){
         $calendar_link ='';
--- pages/index.php
+++ pages/index.php
@@ -3,70 +3,61 @@ session_start();
 require '../bootstrap.php';
 
 if (!isset($_SESSION['user'])) {
-    header('Location: ./login.php');
-    exit();
+  header('Location: ./login.php');
+  exit();
 }
-calendar($_SESSION['user']['edu_group']);
+
 $cal_link = calendar($_SESSION['user']['edu_group']);
 
 echo head('Index');
 ?>
+<style>
+  #calendar {
+    max-width: 900px;
+    margin: 40px auto;
+  }
+</style>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Bienvenue <?php var_dump($_SESSION['user']); ?></h1>
-                <a href="./logout.php">Logout</a>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Bienvenue <?php var_dump($_SESSION['user']); ?></h1>
+        <a href="./logout.php">Logout</a>
+      </div>
     </div>
-    <div id="calendar"></div>
+  </div>
+  <div id="calendar"></div>
 </body>
+
+<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/common@5.11.5/main.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/ical.js@1.5.0/build/ical.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fullcalendar/icalendar@6.1.8/index.global.min.js"></script>
 <script>
-        let calendar = new tui.Calendar('#calendar', {
-        defaultView: 'week',
-        taskView: true,
-        template: {
-            monthDayname: function(dayname) {
-                return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
-            }
-        }
+  document.addEventListener("DOMContentLoaded", function() {
+    const url1 = 'https://corsproxy.io/?' + encodeURIComponent('<?php echo $cal_link; ?>');
+    var calendarEl = document.getElementById("calendar");
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      locale: 'fr',
+      initialView: "timeGridDay",
+      headerToolbar: {
+        left: "prev, today",
+        center: "title",
+        right: "next",
+      },
+      // plugins: [DayGridPlugin, iCalendarPlugin],
+      events: {
+        url: url1,
+        format: "ics",
+      },
+      slotMinTime: '08:00',
+      slotMaxTime: '18:30',
+      hiddenDays: [0, 6],
+      allDaySlot: false,
     });
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '<?php echo $cal_link ?>', true); // Remplacez 'URL_VERS_VOTRE_FICHIER_ICS' par le lien correct vers votre fichier .ics
-    xhr.onload = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var icsData = xhr.responseText;
-
-            // Analyser les événements du fichier .ics
-            var jcalData = ICAL.parse(icsData);
-            var comp = new ICAL.Component(jcalData);
-            var veventList = comp.getAllProperties('vevent');
-
-            // Ajouter les événements au calendrier
-            var schedules = [];
-            for (var i = 0; i < veventList.length; i++) {
-                var vevent = veventList[i];
-                var summary = vevent.getFirstPropertyValue('summary');
-                var startDate = vevent.getFirstPropertyValue('dtstart').toJSDate();
-                var endDate = vevent.getFirstPropertyValue('dtend').toJSDate();
-
-                schedules.push({
-                    calendarId: '1',
-                    id: String(i + 1),
-                    title: summary,
-                    start: startDate,
-                    end: endDate
-                });
-            }
-
-            calendar.clear(); // Effacer les événements existants
-            calendar.createSchedules(schedules); // Ajouter les nouveaux événements
-        }
-    };
-    xhr.send();
-</script>
-<script src="https://cdn.jsdelivr.net/npm/ics@3.2.0/dist/index.min.js"></script>
+    calendar.render();
+  });
 </script>
