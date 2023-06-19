296a7a90d181b34a62565aeac9131f189e68213b
Raphitpt <tiphonetraphi@gmail.com>
2023-06-19 10:59:33 +0200

    change calendar

--- pages/index.php
+++ pages/index.php
@@ -2,9 +2,9 @@
 session_start();
 require '../bootstrap.php';
 
-if(!isset($_COOKIE['jwt'])) {
-    header('Location: ./login.php');
-    exit;
+if (!isset($_COOKIE['jwt'])) {
+  header('Location: ./login.php');
+  exit;
 }
 $jwt = $_COOKIE['jwt'];
 $secret_key = $_ENV['SECRET_KEY']; // Remplacez par votre clé secrète
@@ -35,10 +35,14 @@ echo head('Index');
   <div id="calendar"></div>
 </body>
 
-<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/common@5.11.5/main.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/ical.js@1.5.0/build/ical.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fullcalendar/icalendar@6.1.8/index.global.min.js"></script>
+<!-- <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/common@5.11.5/main.min.css" rel="stylesheet" /> -->
+<script src="
+https://unpkg.com/ical.js@1.5.0/build/ical.js
+"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fullcalendar/icalendar@6.1.8/index.global.min.js"></script> -->
+<link rel="stylesheet" href="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.css" />
+<script src="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.js"></script>
 <script>
   let jwt = localStorage.getItem('jwt');
 
@@ -64,9 +68,68 @@ echo head('Index');
       }
     });
   }
+
   document.addEventListener("DOMContentLoaded", function() {
     const url1 = 'https://corsproxy.io/?' + encodeURIComponent('<?php echo $cal_link; ?>');
-    let calendarEl = document.getElementById("calendar");
+
+    let calendar = new tui.Calendar('#calendar', {
+        defaultView: 'day',
+         // e.g. true, false, or ['allday', 'time']
+        day:{
+          workweek: true,
+          hourStart: 8,
+          hourEnd: 18,
+
+        },
+        week: {
+    taskView: false,
+    scheduleView: ['time'],
+  },
+        template: {
+            monthDayname: function(dayname) {
+                return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+            }
+        }
+    });
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url1, true); // Remplacez 'URL_VERS_VOTRE_FICHIER_ICS' par le lien correct vers votre fichier .ics
+    xhr.onload = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let icsData = xhr.responseText;
+
+            // Analyser les événements du fichier .ics
+            let jcalData = ICAL.parse(icsData);
+            let comp = new ICAL.Component(jcalData);
+            let veventList = comp.getAllSubcomponents('vevent');
+            
+            // Ajouter les événements au calendrier
+            let schedules = [];
+            for (let i = 0; i < veventList.length; i++) {
+                let vevent = veventList[i];
+                let summary = vevent.getFirstPropertyValue('summary');
+                let startDate = vevent.getFirstPropertyValue('dtstart').toJSDate();
+                let endDate = vevent.getFirstPropertyValue('dtend').toJSDate();
+              
+                schedules.push({
+                    calendarId: '1',
+                    id: String(i + 1),
+                    title: summary,
+                    start: startDate,
+                    end: endDate
+                });
+            }
+
+            calendar.clear(); // Effacer les événements existants
+            calendar.createEvents(schedules); // Ajouter les nouveaux événements
+        }
+    };
+    xhr.send();
+    
+  });
+</script>
+<!-- 
+let calendarEl = document.getElementById("calendar");
 
     let calendar = new FullCalendar.Calendar(calendarEl, {
       locale: 'fr',
@@ -87,6 +150,4 @@ echo head('Index');
       allDaySlot: false,
     });
 
-    calendar.render();
-  });
-</script>
+    calendar.render(); -->
