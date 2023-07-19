d9aa928b4b8cd0093a14829db1e944384c24ec63
Raphitpt <tiphonetraphi@gmail.com>
2023-07-19 14:23:45 +0200

    add profil

---
+++ pages/profil.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+require "../bootstrap.php";
+
+
+if (!isset($_COOKIE['jwt'])) {
+    header('Location: ./accueil.php');
+    exit;
+}
+
+$jwt = $_COOKIE['jwt'];
+$secret_key = $_ENV['SECRET_KEY']; // Remplacez par votre clé secrète
+$users = decodeJWT($jwt, $secret_key);
+setlocale(LC_TIME, 'fr_FR.UTF-8'); // Définit la locale en français
+
+if (isset($_POST['submit']) && !empty($_POST['title']) && !empty($_POST['date']) && !empty($_POST['school_subject'])) {
+    $title = $_POST['title'];
+    $date = $_POST['date'];
+    $school_subject = $_POST['school_subject'];
+    $eval = $_POST['eval'];
+    $devoir_rendre = $_POST['devoir_rendre'];
+    $sql = "INSERT INTO agenda (title, date_finish, eval, devoir_rendre, id_user, id_subject) VALUES (:title, :date, :eval, :devoir_rendre, :id_user, :id_subject)";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([
+        'title' => $title,
+        'date' => $date,
+        'id_user' => $users['id_user'],
+        'eval' => $eval,
+        'devoir_rendre' => $devoir_rendre,
+        'id_subject' => $school_subject
+    ]);
+    header('Location: ./agenda.php');
+    exit();
+}
+
+
+$sql_subject = "SELECT * FROM sch_subject";
+$stmt_subject = $dbh->prepare($sql_subject);
+$stmt_subject->execute();
+$subject = $stmt_subject->fetchAll(PDO::FETCH_ASSOC);
+
+echo head("Profil");
+?>
+
+<body class="body-agenda">
+
+    <header>
+        <div class="content_header">
+            <div class="content_title-header">
+                <div class="burger-header" id="burger-header">
+                    <i class="fi fi-br-bars-sort"></i>
+                </div>
+                <div style="width:20px"></div>
+                <h1>Profil</h1>
+            </div>
+        </div>
+
+        <div class="burger_content-header" id="burger_content-header">
+            <div style="height:60px"></div>
+            <div class="burger_content_title-header">
+                <img src="./../assets/img/mmicompanion.svg" alt="">
+                <h1>MMI Companion</h1>
+            </div>
+            <div class="burger_content_content-header">
+                <div class="burger_content_trait_header"></div>
+                <a href="./index.php">
+                    <div class="burger_content_link-header">
+                        <i class="fi fi-br-home"></i>
+                        <p>Vue d'ensemble</p>
+                    </div>
+
+                </a>
+                <a href="./agenda.php">
+                    <div class="burger_content_link-header">
+                        <i class="fi fi-br-calendar"></i>
+                        <p>Agenda</p>
+                        <div class="select_link-header"></div>
+                    </div>
+                </a>
+                <div class="burger_content_trait_header"></div>
+                <a href="./messages.php">
+                    <div class="burger_content_link-header">
+                        <i class="fi fi-br-comment-alt"></i>
+                        <p>Messages</p>
+                    </div>
+                </a>
+                <a href="./mail.php">
+                    <div class="burger_content_link-header">
+                        <i class="fi fi-br-envelope"></i>
+                        <p>Boite mail</p>
+                    </div>
+                </a>
+                <div class="burger_content_trait_header"></div>
+                <a href="./sante.php">
+                    <div class="burger_content_link-header">
+                        <i class="fi fi-br-doctor"></i>
+                        <p>Mon bien être</p>
+                    </div>
+                </a>
+                <a href="./profil.php">
+                    <div class="burger_content_link-header">
+                        <i class="fi fi-br-user"></i>
+                        <p>Mon profil</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </header>
