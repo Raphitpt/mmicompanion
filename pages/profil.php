d09ff294db12005f24ed36c28339fb6f9c90528d
Raphitpt <tiphonetraphi@gmail.com>
2023-07-19 14:48:49 +0200

    test pp link

--- pages/profil.php
+++ pages/profil.php
@@ -13,35 +13,60 @@ $secret_key = $_ENV['SECRET_KEY']; // Remplacez par votre clé secrète
 $users = decodeJWT($jwt, $secret_key);
 setlocale(LC_TIME, 'fr_FR.UTF-8'); // Définit la locale en français
 
-if (isset($_POST['submit']) && !empty($_POST['title']) && !empty($_POST['date']) && !empty($_POST['school_subject'])) {
-    $title = $_POST['title'];
-    $date = $_POST['date'];
-    $school_subject = $_POST['school_subject'];
-    $eval = $_POST['eval'];
-    $devoir_rendre = $_POST['devoir_rendre'];
-    $sql = "INSERT INTO agenda (title, date_finish, eval, devoir_rendre, id_user, id_subject) VALUES (:title, :date, :eval, :devoir_rendre, :id_user, :id_subject)";
+if (isset($_POST['profile-picture'])) {
+        // Récupérer les informations du fichier téléchargé
+    $uploadedFile = $_FILES['profile-picture'];
+    $fileName = $uploadedFile['name'];
+    $fileTempPath = $uploadedFile['tmp_name'];
+
+    // Déplacer le fichier téléchargé vers un emplacement permanent
+    $destinationPath = 'uploads/' . $fileName;
+    move_uploaded_file($fileTempPath, $destinationPath);
+
+    $sql = "UPDATE users SET pp_link = :profile_picture WHERE id_user = :id_user";
     $stmt = $dbh->prepare($sql);
     $stmt->execute([
-        'title' => $title,
-        'date' => $date,
-        'id_user' => $users['id_user'],
-        'eval' => $eval,
-        'devoir_rendre' => $devoir_rendre,
-        'id_subject' => $school_subject
+        'profile_picture' => $destinationPath,
+        'id_user' => $users['id_user']
     ]);
-    header('Location: ./agenda.php');
+    header('Location: ./profil.php');
     exit();
 }
 
-
-$sql_subject = "SELECT * FROM sch_subject";
-$stmt_subject = $dbh->prepare($sql_subject);
-$stmt_subject->execute();
-$subject = $stmt_subject->fetchAll(PDO::FETCH_ASSOC);
+$pp_original = "SELECT pp_link FROM users WHERE id_user = :id_user";
+$stmt_pp_original = $dbh->prepare($pp_original);
+$stmt_pp_original->execute([
+    'id_user' => $users['id_user']
+]);
+$pp_original = $stmt_pp_original->fetch(PDO::FETCH_ASSOC);
 
 echo head("Profil");
 ?>
+  <style>
+    .profile-picture-wrapper {
+      width: 150px;
+      height: 150px;
+      position: relative;
+      overflow: hidden;
+      border-radius: 50%;
+    }
 
+    .profile-picture {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .profile-picture-input {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      cursor: pointer;
+    }
+  </style>
 <body class="body-agenda">
 
     <header>
@@ -107,12 +132,30 @@ echo head("Profil");
         </div>
     </header>
     <main class="main-profil">
+    <form action="" method="post" enctype="multipart/form-data">
     <div class="profile-picture-wrapper">
-    <img id="preview" class="profile-picture" src="placeholder.jpg" alt="Photo de profil">
-    <input id="profile-picture-input" class="profile-picture-input" type="file" onchange="updatePreview(event)">
+      <img id="preview" class="profile-picture" src="<?php echo $pp_original ?>" alt="Photo de profil">
+      <input id="profile-picture-input" class="profile-picture-input" type="file" name="profile-picture" onchange="updatePreview(event)">
+    </div>
+    <input type="submit" value="Télécharger">
+  </form>
+
   </div>
 
     </main>
     <script src="../assets/js/menu-navigation.js"></script>
+    <script>
+        function updatePreview(event) {
+      let input = event.target;
+      let reader = new FileReader();
+
+      reader.onload = function() {
+        let preview = document.getElementById('preview');
+        preview.src = reader.result;
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+    </script>
 </body>
 </html>
