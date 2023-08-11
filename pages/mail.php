f843f8bd098e52f116c35baaee277fa3e0d0dcfd
ArnaudGct <arnaud.graciet@gmail.com>
2023-08-11 16:04:42 +0200

    mail.php

---
+++ pages/mail.php
@@ -0,0 +1,62 @@
+<!-- 
+        Page d'accueil quand on arrive sur l'application via le QR-code ou bien en cliquant sur le lien
+        On peut s'inscrire(register.php) ou se connecter(login.php)
+        On peut aussi accéder à la page d'accueil en cliquant sur le logo en haut à gauche
+-->
+
+<?php
+session_start();
+require "../bootstrap.php";
+
+// si le cookie n'existe pas, on redirige vers la page d'accueil
+if (!isset($_COOKIE['jwt'])) {
+    header('Location: ./accueil.php');
+    exit;
+  }
+  
+// La on récupère le cookie que l'on à crée à la connection, voir login.php et fonction.php
+// --------------------
+$jwt = $_COOKIE['jwt'];
+$secret_key = $_ENV['SECRET_KEY']; // La variable est une variable d'environnement qui est dans le fichier .env
+$user = decodeJWT($jwt, $secret_key);
+setlocale(LC_TIME, 'fr_FR.UTF-8'); // Définit la locale en français mais ne me semble pas fonctionner
+// --------------------
+// Fin de la récupération du cookie
+  
+// On récupère les données de l'utilisateur pour le mail
+$mail_user = $user['edu_mail'];
+$id_user = $user['id_user'];
+
+// SQL INSTRUCTIONS
+$sql = "SELECT * FROM users WHERE id_user = :id_user and edu_mail = :mail_user";
+$stmt_sql = $dbh->prepare($sql);
+$stmt_sql->execute([
+    'id_user' => $id_user,
+    'edu_mail' => $mail_user
+]);
+$sql = $stmt_sql->fetch(PDO::FETCH_ASSOC);
+dd($sql);
+
+
+
+echo head("MMI Companion - Vérification du mail");
+?>
+<body class="body-mail">
+    <main class="main-mail">
+        <div class="illustration-mail">
+            <img src="../assets/img/verif_mail.svg" alt="Illustration diverse">
+        </div>
+        <div class="title-mail">
+            <h1>Vérifie tes mails</h1>
+            <p>Un mail vient d’être envoyé à <span style="font-weight: 600;"><?php echo $mail_user; ?></span> pour vérifier votre adresse et activer votre compte.</p>
+        </div>
+        <div style="height:20px"></div>
+        <div class="trait-mail"></div>
+        <div style="height:20px"></div>
+
+        <div class="button-accueil">
+            <a role="button" href="./register.php" class="button_register">Renvoyer un mail</a>
+        </div>
+    </main>
+</body>
+</html>
