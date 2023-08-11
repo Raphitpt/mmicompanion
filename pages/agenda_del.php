0abbcd535d9714acc4ae91f56c3c1cf2f938ab5d
Raphitpt <tiphonetraphi@gmail.com>
2023-08-11 12:27:15 +0200

    Add suppression sur l'agenda

---
+++ pages/agenda_del.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+require "../bootstrap.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id_user']) && isset($_GET['id_task'])) {
+    $id_user = $_GET['id_user'];
+    $id_task = $_GET['id_task'];
+    $sql = "DELETE FROM agenda WHERE id_task = :id_task AND id_user = :id_user";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([
+        'id_task' => $id_task,
+        'id_user' => $id_user
+    ]);
+    header('Location: ../agenda.php');
+    exit();
+}
+else{
+    header('Location: ../agenda.php');
+    exit();
+}
