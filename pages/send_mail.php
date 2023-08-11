1e14b0719f8d171f0816d7c740983f19ea0e4eae
ArnaudGct <arnaud.graciet@gmail.com>
2023-08-11 17:53:57 +0200

    fix mail

--- pages/send_mail.php
+++ pages/send_mail.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 require '../bootstrap.php';
-
+if((isset($_POST['mail_user'])) && !empty($_POST['verif_code']))
 $mail_user = strip_tags($_POST['mail_user']);
 $verif_code = strip_tags($_POST['verif_code']);
 
