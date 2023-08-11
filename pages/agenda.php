6654bc99f5702c50f989cf9e5b37d74ce688425f
ArnaudGct <arnaud.graciet@gmail.com>
2023-08-11 15:25:23 +0200

    comment

--- pages/agenda.php
+++ pages/agenda.php
@@ -265,9 +265,11 @@ echo head("MMI Companion - Agenda");
                     echo "</div>";
                     echo "<div class='agenda_content_list_item_flexright-agenda'>";
                     echo "<i class='fi fi-br-pencil blue'></i>";
+                    // Ne pas afficher la corbeille si l'utilisateur est un étudiant et que c'est une évaluation
                     if($agenda['type'] == "eval" && $users['role'] = "etudiant"){
                         echo "<i class='fi fi-br-trash red' hidden></i>";
-                    } else {
+                    } 
+                    else {
                         echo "<a href='agenda_del.php/?id_user=".$users['id_user']."&id_task=".$agenda['id_task']."'><i class='fi fi-br-trash red'></i></a>";
                     }
 
