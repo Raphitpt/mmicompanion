6e9ea1934c88dfedadf722e55f9179bb6ea11b4b
ArnaudGct <arnaud.graciet@gmail.com>
2023-09-01 13:17:11 +0200

    modif name

--- pages/reset_password.php
+++ pages/reset_password.php
@@ -11,8 +11,12 @@ if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['email']) && isset($_GET[
         'edu_mail' => $edu_mail,
     ]);
     $user = $stmt_code->fetch(PDO::FETCH_ASSOC);
-    if($user['verification_code_mail'] == $activation_code){ ?>
-    <main>
+    if($user['verification_code_mail'] == $activation_code) { 
+
+?>
+
+<body class="body-login">
+    <main class="main-login">
         <form method="POST">
             <input type="hidden" name="mail_user" value="<?php echo $edu_mail; ?>">
             <input type="password" name="password" placeholder="Nouveau mot de passe">
@@ -20,31 +24,32 @@ if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['email']) && isset($_GET[
             <input type="submit" name="submit" value="Valider">
         </form>
     </main>
+</body>
         
-    <?php }
-    if(isset($_POST['submit'])){
-        $password = $_POST['password'];
-        $password_confirm = $_POST['password_confirm'];
-        if($password == $password_confirm){
-            $password = password_hash($password, PASSWORD_DEFAULT);
-            $sql = "UPDATE users SET password = :password WHERE edu_mail = :edu_mail";
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute([
-                'password' => $password,
-                'edu_mail' => $edu_mail
-            ]);
-            header('Location: ./login.php');
-            exit();
-        }
-        else{
-            header('Location: ./reset_pass.php');
-            $_SESSION['erreur'] = "Les mot de passe ne correspondent pas !";
-            exit();
-        }
-    }
-}
+<?php }
 
-    else{
-        header('Location: ./accueil.php');
+if(isset($_POST['SUBMIT'])){
+    $password = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+    if($password == $password_confirm){
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "UPDATE users SET password = :password WHERE edu_mail = :edu_mail";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([
+            'password' => $password,
+            'edu_mail' => $edu_mail
+        ]);
+        header('Location: ./login.php');
         exit();
     }
+    else{
+        header('Location: ./reset_password.php');
+        $_SESSION['erreur'] = "Les mot de passe ne correspondent pas !";
+        exit();
+    }
+}
+}
+else{
+    header('Location: ./accueil.php');
+    exit();
+}
