2d4e6fb7a910e012b293b477156557686b0e007c
Raphaël Tiphonet <43700748+Raphitpt@users.noreply.github.com>
2023-09-05 16:38:14 +0200

    Optimiser le temps de chargement

--- Helpers/functions.php
+++ Helpers/functions.php
@@ -82,10 +82,6 @@ function head(string $title = ''): string
 <link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="../splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png">
 <link rel="apple-touch-startup-image" media="screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="../splash_screens/8.3__iPad_Mini_portrait.png">
   <title>$title</title>
-  <link rel="stylesheet" href="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.css" />
-    <script src="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.js"></script>
-    
-
 </head>
 
 HTML_HEAD;
