c9bbb443b62b601abeca383f3c95c666b2327ee0
Raphitpt <tiphonetraphi@gmail.com>
2023-09-20 21:21:52 +0200

    fix mail test

--- test_mail.php
+++ test_mail.php
@@ -2,7 +2,7 @@
 
 require 'bootstrap.php';
 
-$ve = new hbattat\VerifyEmail('fzgdsfhjfd.sfdfddsf@etu.univ-poitiers.fr', 'no-reply@mmi-companion.fr');
+$ve = new hbattat\VerifyEmail('raphael.tiphonet@etu.univ-poitiers.fr', 'no-reply@mmi-companion.fr');
 
 var_dump($ve->verify());
 print_r($ve->get_errors());
