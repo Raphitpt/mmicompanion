7cc474e774eb94f559a7865098245c1e1636b992
Raphitpt <tiphonetraphi@gmail.com>
2023-09-20 21:39:45 +0200

    Correction de bug et ajout de prof

--- test_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'bootstrap.php';
-
-$ve = new hbattat\VerifyEmail('raphael.tiphonet@etu.univ-poitiers.fr', 'no-reply@mmi-companion.fr');
-
-var_dump($ve->verify());
-print_r($ve->get_errors());
-print_r($ve->get_debug());
