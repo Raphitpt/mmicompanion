07a1f4a0c6ed7f92c8e803812754283c509b22f0
Arnaud Graciet <120825822+ArnaudGct@users.noreply.github.com>
2023-09-21 00:24:27 +0200

    Update information_edit.php

--- pages/information_edit.php
+++ pages/information_edit.php
@@ -44,10 +44,12 @@ session_start();
 
 if (isset($_POST['submit'])) {
     if(!empty($_POST['titre']) && !empty($_POST['user']) && !empty($_POST['content']) && !empty($_POST['group_info'])){
-    $json_group_info = json_decode($_POST['group_info']);
-    $group_info = $json_group_info;
-    $group_info = implode(',', $group_info);
-
+    if (!empty($_POST['group_info'])) {
+        $json_group_info = json_decode($_POST['group_info']);
+        $group_info = $json_group_info;
+        $group_info = implode(',', $group_info);
+    }
+    
     $title = $_POST['titre'];
     $name = $_POST['user'];
     $user_role = $_POST['role'];
