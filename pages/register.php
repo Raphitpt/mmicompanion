5836ef855a466dff32e0428caad78e1d450f5a12
Raphitpt <tiphonetraphi@gmail.com>
2023-09-20 21:30:25 +0200

    fix error message

--- pages/register.php
+++ pages/register.php
@@ -119,10 +119,10 @@ echo head('MMI Companion | Register');
                 <div class="trait_register"></div>
                 <input type="submit" value="Créer mon compte" class="button_register">
                 <div style="height:15px"></div>
-                <?php if(!empty($_SESSION['error_message'])) { ?>
-                    <div class="error_message-login"><?php echo $_SESSION['error_message']; ?></div>
+                <?php if(!empty($error_message)) { ?>
+                    <div class="error_message-login"><?php echo $error_message ?></div>
                 <?php
-                unset($_SESSION['error_message']);
+                // unset($_SESSION['error_message']);
             } ?>
             </div>
         </form>
