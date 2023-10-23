878b888a0fbca3076ebdd6442bbd0918c1095d47
Raphitpt <tiphonetraphi@gmail.com>
2023-10-23 20:38:17 +0200

    edit strawpoll

--- assets/js/strawpoll.js
+++ assets/js/strawpoll.js
@@ -1,174 +1,211 @@
 (function ($) {
-    'use strict';
+  "use strict";
 
-    // Plugin default options
-    var defaultOptions = {
-    };
+  // Plugin default options
+  var defaultOptions = {};
 
-    // If the plugin is a button
-    function buildButtonDef(trumbowyg) {
-        return {
-            fn: function () {
-                // Créez une fenêtre modale de Trumbowyg pour saisir les détails du StrawPoll
-                var modalContent = '<label>Question : <input id="pollQuestion" type="text"></label>';
-                modalContent += '<label>Options :';
-                modalContent += '<input class="option-input" type="text">';
-                modalContent += '<input class="option-input" type="text">';
-                modalContent += '<button id="addOption">Ajouter une option</button>';
-                modalContent += '</label>';
+  // If the plugin is a button
+  function buildButtonDef(trumbowyg) {
+    return {
+      fn: function () {
+        // Créez une fenêtre modale de Trumbowyg pour saisir les détails du StrawPoll
+        var modalContent =
+          '<label>Question : <input id="pollQuestion" type="text"></label>';
+        modalContent += "<label>Options :";
+        modalContent += '<input class="option-input" type="text">';
+        modalContent += '<input class="option-input" type="text">';
+        modalContent += '<button id="addOption">Ajouter une option</button>';
+        modalContent += "</label>";
 
-                modalContent += '<button id="customConfirm">Créer ce sondage</button>';
-                modalContent += '<button id="customCancel">Annuler</button>';
+        modalContent += '<button id="customConfirm">Créer ce sondage</button>';
+        modalContent += '<button id="customCancel">Annuler</button>';
 
-                trumbowyg.openModal('Saisir les détails du StrawPoll', modalContent, function () {
-                    var pollQuestion = $('#pollQuestion').val();
-                    if (!pollQuestion) return;
+        trumbowyg.openModal(
+          "Saisir les détails du StrawPoll",
+          modalContent,
+          function () {
+            var pollQuestion = $("#pollQuestion").val();
+            if (!pollQuestion) return;
 
-                    // Récupérez les options
-                    var pollOptions = [];
-                    $('.option-input').each(function () {
-                        var optionText = $(this).val();
-                        if (optionText.trim() !== '') {
-                            pollOptions.push(optionText);
-                        }
-                    });
+            // Récupérez les options
+            var pollOptions = [];
+            $(".option-input").each(function () {
+              var optionText = $(this).val();
+              if (optionText.trim() !== "") {
+                pollOptions.push(optionText);
+              }
+            });
 
-                    // Gérer la création du StrawPoll
-                    var pollData = {
-                        title: pollQuestion,
-                        poll_options: pollOptions,
-                        type: 'multiple_choice', // Vous pouvez modifier le type ici
-                        // Ajoutez d'autres champs de données en fonction de vos besoins
-                    };
+            // Gérer la création du StrawPoll
+            var pollData = {
+              title: pollQuestion,
+              poll_options: pollOptions,
+              type: "multiple_choice", // Vous pouvez modifier le type ici
+              // Ajoutez d'autres champs de données en fonction de vos besoins
+            };
 
-                    // Paramètres pour la requête POST
-                    var settings = {
-                        async: true,
-                        crossDomain: true,
-                        url: 'https://api.strawpoll.com/v3/polls',
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'X-API-Key': '123' // Remplacez par votre clé API
-                        },
-                        processData: false,
-                        data: JSON.stringify(pollData),
-                    };
+            // Créez une nouvelle instance de XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+            var url = "https://api.strawpoll.com/v3/polls";
 
-                    // Faire une requête POST vers l'API StrawPoll
-                    $.ajax(settings)
-                        .done(function (response) {
-                            // Insérer le StrawPoll dans l'éditeur
-                            var strawPollHtml = '<iframe src="' + response.url + '"></iframe>';
-                            trumbowyg.execCmd('insertHTML', strawPollHtml);
-                        })
-                        .fail(function (error) {
-                            console.error('Erreur lors de la création du StrawPoll : ' + error);
-                        });
+            // Configurez la requête POST
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader(
+              "X-API-Key",
+              "e2357040-71cb-11ee-ba08-a87f99aa68de"
+            ); // Remplacez par votre clé API
 
-                    // Fermez la fenêtre modale
-                    trumbowyg.closeModal();
-                });
+            // Gérez l'événement de réponse
+            xhr.onload = function () {
+              if (xhr.status === 200) {
+                // Insérer le StrawPoll dans l'éditeur
+                var response = JSON.parse(xhr.responseText);
+                var strawPollHtml =
+                  '<iframe src="' + response.url + '"></iframe>';
+                trumbowyg.execCmd("insertHTML", strawPollHtml);
+              } else {
+                console.error(
+                  "Erreur lors de la création du StrawPoll : " + xhr.status
+                );
+              }
 
-                // Gérer l'ajout de champs d'option
-                $('#addOption').on('click', function () {
-                    var newOptionInput = '<input class="option-input" type="text">';
-                    $('.option-input:last').after(newOptionInput);
-                });
+              // Fermez la fenêtre modale
+              trumbowyg.closeModal();
+            };
 
-                // Gérer l'événement "Annuler"
-                $('#customCancel').on('click', function () {
-                    // Fermez la fenêtre modale
-                    trumbowyg.closeModal();
-                });
+            // Gérez l'envoi des données
+            xhr.send(JSON.stringify(pollData));
+          }
+        );
 
-                // Gérer l'événement "Confirmer"
-                $('#customConfirm').on('click', function () {
-                    console.log('Créer un StrawPoll');
-                    var pollQuestion = $('#pollQuestion').val();
-                    if (!pollQuestion) return;
-                
-                    // Récupérez les options
-                    var pollOptions = [];
-                    $('.option-input').each(function () {
-                        var optionText = $(this).val();
-                        if (optionText.trim() !== '') {
-                            pollOptions.push(optionText);
-                        }
-                    });
-                
-                    // Gérer la création du StrawPoll
-                    var pollData = {
-                        title: pollQuestion,
-                        poll_options: pollOptions,
-                        type: 'multiple_choice', // Vous pouvez modifier le type ici
-                        // Ajoutez d'autres champs de données en fonction de vos besoins
-                    };
-                
-                    // Paramètres pour la requête POST
-                    var settings = {
-                        async: true,
-                        crossDomain: true,
-                        url: 'https://api.strawpoll.com/v3/polls',
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'X-API-Key': 'e2357040-71cb-11ee-ba08-a87f99aa68de' // Remplacez par votre clé API
-                        },
-                        processData: false,
-                        data: JSON.stringify(pollData),
-                    };
-                
-                    // Faire une requête POST vers l'API StrawPoll
-                    $.ajax(settings).done(function (response) {
-                        console.log(response);
-                      });
-                });
-                
+        // Gérer l'ajout de champs d'option
+        $("#addOption").on("click", function () {
+          var newOptionInput = '<input class="option-input" type="text">';
+          $(".option-input:last").after(newOptionInput);
+        });
+
+        // Gérer l'événement "Annuler"
+        $("#customCancel").on("click", function () {
+          // Fermez la fenêtre modale
+          trumbowyg.closeModal();
+        });
+
+        // Gérer l'événement "Confirmer"
+        $("#customConfirm").on("click", function () {
+          console.log("Créer un StrawPoll");
+          var pollQuestion = $("#pollQuestion").val();
+          if (!pollQuestion) return;
+
+          // Récupérez les options
+          var pollOptions = [];
+          $(".option-input").each(function () {
+            var optionText = $(this).val();
+            if (optionText.trim() !== "") {
+              pollOptions.push(optionText);
             }
-        };
+          });
+
+          // Gérer la création du StrawPoll
+          var pollData = {
+            title: pollQuestion,
+            poll_options: pollOptions,
+            type: "multiple_choice", // Vous pouvez modifier le type ici
+            // Ajoutez d'autres champs de données en fonction de vos besoins
+          };
+          const data = JSON.stringify(pollData);
+
+          const xhr = new XMLHttpRequest();
+          xhr.withCredentials = true;
+
+          xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+              console.log(this.responseText);
+            }
+          });
+
+          xhr.open("POST", "https://api.strawpoll.com/v3/polls");
+          xhr.setRequestHeader("Content-Type", "application/json");
+          xhr.setRequestHeader(
+            "X-API-Key",
+            "e2357040-71cb-11ee-ba08-a87f99aa68de"
+          );
+
+          xhr.send(data);
+
+          // Gérez l'événement de réponse
+          xhr.onload = function () {
+            if (xhr.status === 200) {
+              // Insérer le StrawPoll dans l'éditeur
+              var response = JSON.parse(xhr.responseText);
+              var strawPollHtml =
+                '<iframe src="' + response.url + '"></iframe>';
+              trumbowyg.execCmd("insertHTML", strawPollHtml);
+            } else {
+              console.error(
+                "Erreur lors de la création du StrawPoll : " + xhr.status
+              );
+            }
+
+            // Fermez la fenêtre modale
+            trumbowyg.closeModal();
+          };
+
+          // Gérez l'envoi des données
+          xhr.send(JSON.stringify(pollData));
+        });
+      },
+    };
+  }
+
+  // If the plugin is a button
+  function buildButtonIcon() {
+    if ($("#trumbowyg-myplugin").length > 0) {
+      return;
     }
 
-    // If the plugin is a button
-    function buildButtonIcon() {
-        if ($("#trumbowyg-myplugin").length > 0) {
-            return;
-        }
+    const iconWrap = $(
+      document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    );
+    iconWrap.addClass("trumbowyg-icons");
 
-        const iconWrap = $(document.createElementNS("http://www.w3.org/2000/svg", "svg"));
-        iconWrap.addClass("trumbowyg-icons");
-
-        // Icône personnalisée pour le bouton "StrawPoll"
-        iconWrap.html(`
+    // Icône personnalisée pour le bouton "StrawPoll"
+    iconWrap
+      .html(
+        `
       <symbol id="trumbowyg-myplugin" viewBox="0 0 24 24">
         <path d="M21 8v12.993A1 1 0 0 1 20.007 22H3.993A.993.993 0 0 1 3 21.008V2.992C3 2.455 3.449 2 4.002 2h10.995L21 8zm-2 1h-5V4H5v16h14V9zM8 7h3v2H8V7zm0 4h8v2H8v-2zm0 4h8v2H8v-2z"/>
       </symbol>
-    `).appendTo(document.body);
-    }
+    `
+      )
+      .appendTo(document.body);
+  }
 
-    $.extend(true, $.trumbowyg, {
-        // Add some translations
-        langs: {
-            en: {
-                myplugin: 'My plugin'
-            }
+  $.extend(true, $.trumbowyg, {
+    // Add some translations
+    langs: {
+      en: {
+        myplugin: "My plugin",
+      },
+    },
+    // Register plugin in Trumbowyg
+    plugins: {
+      myplugin: {
+        // Code called by Trumbowyg core to register the plugin
+        init: function (trumbowyg) {
+          // Fill current Trumbowyg instance with the plugin default options
+          trumbowyg.o.plugins.myplugin = $.extend(
+            true,
+            {},
+            defaultOptions,
+            trumbowyg.o.plugins.myplugin || {}
+          );
+
+          // If the plugin is a button
+          buildButtonIcon();
+          trumbowyg.addBtnDef("strawpoll", buildButtonDef(trumbowyg));
         },
-        // Register plugin in Trumbowyg
-        plugins: {
-            myplugin: {
-                // Code called by Trumbowyg core to register the plugin
-                init: function (trumbowyg) {
-                    // Fill current Trumbowyg instance with the plugin default options
-                    trumbowyg.o.plugins.myplugin = $.extend(true, {},
-                        defaultOptions,
-                        trumbowyg.o.plugins.myplugin || {}
-                    );
-
-                    // If the plugin is a button
-                    buildButtonIcon();
-                    trumbowyg.addBtnDef('strawpoll', buildButtonDef(trumbowyg));
-                }
-            }
-        }
-    });
+      },
+    },
+  });
 })(jQuery);
--- pages/informations_add.php
+++ pages/informations_add.php
@@ -165,6 +165,8 @@ echo head('MMI Companion | Informations');
     <script src="../assets/js/tree.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
     <!-- <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script> -->
     <script src="./../trumbowyg/dist/trumbowyg.min.js"></script>
     <script src="./../assets/js/strawpoll.js"></script>
