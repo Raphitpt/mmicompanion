63dedb0eb4302587d292233aab9058dfc5bb1498
Raphitpt <tiphonetraphi@gmail.com>
2023-11-13 18:48:39 +0100

    Fix push notification badge count

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
 self.addEventListener("install", () => {
-	self.skipWaiting();
-  });
-  
-  self.addEventListener("push", (event) => {
-	const data = event.data ? event.data.json() : {};
-  const unreadCount = message.unreadCount;
+  self.skipWaiting();
+});
 
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : {};
+  const unreadCount = data.unreadCount; // Utilisez data.unreadCount au lieu de message.unreadCount
 
-	event.waitUntil(
+  event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
+    })
+  );
 
-    }));
-
-    // set or update the badge
-    if (navigator.setAppBadge) {
-      if (unreadCount && unreadCount > 0) {
-        navigator.setAppBadge(unreadCount);
-      } else {
-        navigator.clearAppBadge();
-      }
+  // set or update the badge
+  if (navigator.setAppBadge) {
+    if (unreadCount && unreadCount > 0) {
+      navigator.setAppBadge(unreadCount);
+    } else {
+      navigator.clearAppBadge();
     }
-  });
+  }
+});
