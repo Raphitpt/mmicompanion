f68e4033f518a62fcb0e54be70697e134816d443
Raphaël Tiphonet <43700748+Raphitpt@users.noreply.github.com>
2023-11-30 13:44:18 +0100

    Update absence_get.php

--- pages/absence_get.php
+++ pages/absence_get.php
@@ -38,8 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['semestre']) && !empty(
 
     // sauvegarde les logs
     curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_STDERR, $fp);
-    $fp = fopen(dirname(__DIR__).'/errorlog.txt', 'w');
+    curl_setopt($ch, CURLOPT_STDERR, fopen(dirname(__DIR__).'/errorlog.txt', 'w'));
 
     if (curl_error($ch)) {
         $error_msg = curl_error($ch);
