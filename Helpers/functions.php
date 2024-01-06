1e101ed6002391342084644bbd2cf2fb1cc0b6ae
ArnaudGct <arnaud.graciet@gmail.com>
2024-01-06 09:48:52 +0100

    new stylesheet for theme custom

--- Helpers/functions.php
+++ Helpers/functions.php
@@ -44,8 +44,9 @@ function head(string $title = '', string $additionalStyles = ''): string
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <link rel="manifest" href="../manifest.json">
 
-  <link href="../assets/css/style.css?v=2.21" rel="stylesheet"">
-  <link href="../assets/css/responsive.css" rel="stylesheet"">
+  <link href="../assets/css/style.css?v=2.21" rel="stylesheet">
+  <link href="../assets/css/responsive.css" rel="stylesheet">
+  <link href="../assets/css/style_theme.css" rel="stylesheet">
   <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.1.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
   <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.1.0/uicons-bold-rounded/css/uicons-bold-rounded.css'>
   $additionalStyles
