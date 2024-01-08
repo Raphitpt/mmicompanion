cdc155ded14d1a4c316158997bf1708415f499ba
Raphitpt <tiphonetraphi@gmail.com>
2024-01-08 17:50:21 +0100

    Update meta viewport in functions.php

--- Helpers/functions.php
+++ Helpers/functions.php
@@ -44,7 +44,7 @@ function head(string $title = '', string $additionalStyles = ''): string
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="icon" type="image/svg" href="../assets/img/mmicompanion_512.svg" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="initial-scale=1,user-scalable=no,viewport-fit=cover" />
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
