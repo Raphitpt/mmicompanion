30a48e6dde8c68ff8bd03a0ea5a60ce5751243b4
Raphitpt <tiphonetraphi@gmail.com>
2024-01-29 18:23:00 +0100

    Add access restriction to sendIdProf.php

--- pages/sendIdProf.php
+++ pages/sendIdProf.php
@@ -2,6 +2,12 @@
 session_start();
 require('../bootstrap.php');
 
+$user = onConnect($dbh);
+if (str_contains($user['role'], 'admin') == false && ($user['edu_mail'] != 'raphael.tiphonet@etu.univ-poitiers.fr' || $user['edu_mail'] != 'arnaud.graciet@etu.univ-poitiers.fr')) {
+    header('Location: ./../index.php');
+    exit;
+}
+
 if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['mail']) && isset($_GET['pname']) && isset($_GET['name']) && isset($_GET['trigramme'])) {
     $mail = $_GET['mail'];
     $pname = $_GET['pname'];
