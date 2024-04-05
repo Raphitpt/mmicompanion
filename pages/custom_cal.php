e47b5fdc5aa967b22e815977bab39592f6c0a8d0
Raphaël Tiphonet <43700748+Raphitpt@users.noreply.github.com>
2024-04-05 15:28:14 +0200

    Update custom_cal.php

--- pages/custom_cal.php
+++ pages/custom_cal.php
@@ -98,8 +98,8 @@ function findProf($profName){
                         $eventEnd = $event->DTEND->getDateTime();
 
                         // Ajoutez 2 heures à l'heure de début et de fin
-                        $eventStart = $eventStart->add(new DateInterval('PT1H'));
-                        $eventEnd = $eventEnd->add(new DateInterval('PT1H'));
+                        $eventStart = $eventStart->add(new DateInterval('PT2H'));
+                        $eventEnd = $eventEnd->add(new DateInterval('PT2H'));
 
                         // Accédez à la description
                         $descriptionObject = $event->DESCRIPTION;
